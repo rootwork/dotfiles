cb14efb9a5219b61077bdabebd8bb8256528ef64
Ivan Boothe <git@rootwork.org>
2021-12-09 17:34:26 -0800

    add simple image resizing script
    Signed-off-by: Ivan Boothe <git@rootwork.org>

---
+++ scripts/theirs/resize.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# resize.sh - Use imagemagick to resize an image to a specific width, setting
+#             the height proportionally.
+# Source: https://stackoverflow.com/a/7201084
+
+USAGE="resize.sh imagefile width"
+
+if [ "$#" == "0" ]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+if [ -e "$1" ]; then
+  file="$1"
+  name="${1%.*}"
+  ext="${1##*.}"
+else
+  echo "File not found: $1"
+  exit 1
+fi
+
+if [ ! -z "$2" ]; then
+  width="$2"
+else
+  echo "Need width (in pixels)."
+  exit 1
+fi
+
+convert=$(command -v convert)
+if [[ ! $convert ]]; then
+  error_exit "Imagemagick must be installed. Aborting."
+fi
+
+"${convert}" "${file}" -resize "${width}"x "${name}_${width}.${ext}"
